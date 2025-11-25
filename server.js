93209b065e7ca58ef3ccc2d5cc42644c804fa160
netnetnet023 <aaronmolayal@gmail.com>
2025-11-26 09:26:30 +1100

    hhhh

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import pg from 'pg';
 import cors from 'cors';
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
 app.use(cors());
@@ -20,6 +23,7 @@ app.get('/products', async (req, res) => {
   res.json(result.rows);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
