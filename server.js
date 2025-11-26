2e9ebf3acefdefcb3b02c71976d5858e599a67a1
netnetnet023 <aaronmolayal@gmail.com>
2025-11-26 19:28:01 +1100

    dddddl

--- server.js
+++ server.js
@@ -107,8 +107,13 @@ app.delete('/products/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
+});
+
+
+app.use((err, req, res, next) => {
+  console.error("Internal server error:", err);
+  res.status(500).json({ error: "Something went wrong" });
 });
